1664eaa9dfad80e93c1c806c5540d56119622170
verisorry <59988812+verisorry@users.noreply.github.com>
2022-07-28 22:27:35 +0800

    finished exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  return Math.round(((temperature - 32) * (5/9))*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  return Math.round((temperature*(9/5)+32)*10)/10;
 };
 
 // Do not edit below this line
