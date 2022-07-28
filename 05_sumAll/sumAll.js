76632990cf889267704cda60ed9cddad5bb8f852
verisorry <59988812+verisorry@users.noreply.github.com>
2022-07-28 22:08:44 +0800

    finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0 || typeof start != "number"|| typeof end != "number") {
+        return "ERROR";
+    }
+    
+    var range = [start, end];
+    var result = 0;
+    range.sort();
+    for (i = range[0]-1; i < range[1]; i++) {
+        result += range[0]+i;
+    }
+    return result;
 };
 
 // Do not edit below this line
