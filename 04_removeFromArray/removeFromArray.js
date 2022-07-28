9d440ca90827323578c8e6e065f329a107f228dc
verisorry <59988812+verisorry@users.noreply.github.com>
2022-07-28 17:53:58 +0800

    finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const arrayArgs = Array.from(args);
+    var array = arrayArgs.shift();
 
+    for (const target of arrayArgs) {
+        const idx = array.indexOf(target);
+        if (idx > -1) {
+            array.splice(idx, 1);
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
