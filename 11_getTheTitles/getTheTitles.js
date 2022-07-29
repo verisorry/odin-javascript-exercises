55b59b3d62e7a897bd04fc3ff1e7de80ad4367cb
verisorry <59988812+verisorry@users.noreply.github.com>
2022-07-29 11:46:15 +0800

    finished exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let array = []
+    for (const dict of books) {
+        array.push(dict['title']);
+    }
+    return array;
 };
 
 // Do not edit below this line
