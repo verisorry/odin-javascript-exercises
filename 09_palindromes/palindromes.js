79416f3adf61c08969e07da2714b2af82b96e8cc
verisorry <59988812+verisorry@users.noreply.github.com>
2022-07-29 11:31:13 +0800

    finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.replace(/[^\w\s']|_/g, "").replace(/\s+/g, "").toLowerCase().split("");
+    if (string.length % 2 == 0) {
+        if (string.slice(0, Math.floor(string.length/2)).join("") === (string.slice(Math.floor(string.length/2), string.length).reverse()).join("")) {
+            return true;
+        }
 
+    } else {
+        if (string.slice(0, Math.floor(string.length/2)).join("") === string.slice(Math.floor(string.length/2)+1, string.length).reverse().join("")) {
+            return true;
+        }
+    } 
+    return false;
 };
 
 // Do not edit below this line
