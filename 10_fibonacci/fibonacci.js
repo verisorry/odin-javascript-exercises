78d83b0f47e96ebcd42747fdf28af1544ea86218
verisorry <59988812+verisorry@users.noreply.github.com>
2022-07-29 11:43:16 +0800

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = parseInt(number);
+    if (number < 0) {
+        return "OOPS";
+    }
+    if (number <= 1) {
+        return number;
+    } return (fibonacci(number-1) + fibonacci(number-2));
 
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
