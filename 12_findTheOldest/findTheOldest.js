890993a391fc6bab4189f63a04d147d2f880e059
verisorry <59988812+verisorry@users.noreply.github.com>
2022-07-29 11:57:44 +0800

    finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let ages = [];
+    for (const person of people) {
+        if (!person["yearOfDeath"]) {
+            const d = new Date();
+            person["yearOfDeath"] = d.getFullYear();
+        }
+        ages.push(person["yearOfDeath"] - person["yearOfBirth"])
+    }
+    let currentMax = 0; 
+    let maxIdx = 0;
+    for (idx = 0; idx < ages.length; idx++) {
+        if (ages[idx] > currentMax) {
+            maxIdx = idx;
+            currentMax = ages[idx];
+        }
+    }
+    return people[maxIdx];
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
