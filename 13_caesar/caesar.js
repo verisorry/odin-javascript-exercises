7553fa3bbb25814a2914dc560346b96cf6a3e549
verisorry <59988812+verisorry@users.noreply.github.com>
2022-07-29 12:39:44 +0800

    finished exercise 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,20 @@
-const caesar = function() {
-
+const caesar = function(string, shift) {
+    result = "";
+    for (let letter of string) {
+        if (letter.match(/[a-zA-Z]/i)) {
+            if (shift < 0) {
+                shift = 26 + (shift % 26);
+            }
+            if (letter.charCodeAt(0) <97) {
+                letter = (letter.charCodeAt(0) - 65 + shift) % 26 + 65;
+            } else {
+                letter = 97 + (letter.charCodeAt(0) + shift) % 97 % 26;
+            }
+            letter = String.fromCharCode(letter);
+        }
+        result += letter;
+    }
+    return result;
 };
 
 // Do not edit below this line
